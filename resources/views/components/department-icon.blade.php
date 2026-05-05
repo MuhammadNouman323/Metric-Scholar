718593ec488b2b74e8937e6e423550cccaab1160
NOUMAN <bc220410458mno@vu.edu.pk>
2026-05-05 10:49:16 +0500

    department Icon Added

---
+++ resources/views/components/department-icon.blade.php
@@ -0,0 +1,3 @@
+<div class="department-icon inline-flex items-center justify-center text-[#0e48c1] {{ $class ?? '' }}" data-department="{{ $department }}" style="display: inline-flex; align-items: center; justify-content: center;">
+    {!! $svg !!}
+</div>
