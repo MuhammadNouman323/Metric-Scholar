fcf0b8c8bdd19012df5ca5e256afa288a5a3f86a
MuhammadNouman323 <bc220410458mno@vu.edu.pk>
2026-08-25 23:03:06 +0500

    favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/jpeg" href="{{ asset('favicon.jpg') }}">
 
     <title>{{ $title ?? 'Scholar Metric' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
